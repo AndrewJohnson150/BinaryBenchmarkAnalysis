1367a56d459d47d49f7c840e5c5aa1d6d2a8a1cf
Andrew Johnson <andrewjohnson150@gmail.com>
2021-09-03 10:35:24 -0600

    Adding up to date progress

--- Analysis/ModelFitting.R
+++ Analysis/ModelFitting.R
@@ -16,6 +16,7 @@ library(MASS)
 library(pscl)
 library(car)
 library(effects)
+library(AER)
 
 data = read.csv("Data/DataWithDomainNoDependency.csv")
 
@@ -34,13 +35,15 @@ data[,"yrFindings"]<- ifelse(data[,"yara_rules"]==0,0,1)
 data[,"cbtFindings"]<- as.factor(data[,"cbtFindings"])
 data[,"yrFindings"]<- as.factor(data[,"yrFindings"])
 
+
+
 #CWE_Checker Model fitting:############################################
-lm1<- glm(log(cwe_checker) ~ log(size) +domain+compiler+static,data=data[,])
+lm1<- lm(log(cwe_checker) ~ log(size) +domain+compiler+static,data=data)
 summary(lm1)
 par(mfrow=c(2,2))
 plot(lm1)
 #Removed non-sig variables removing by lowest coefficient
-lm1<- glm(log(cwe_checker) ~ log(size) + static,data=data[,])
+lm1<- lm(log(cwe_checker) ~ log(size) + static,data=data[,])
 summary(lm1)
 par(mfrow=c(2,2))
 plot(lm1)
@@ -55,47 +58,64 @@ summary(poisglm2)
 par(mfrow=c(2,2))
 plot(poisglm2)
 plot(allEffects(poisglm2,resid=T),type="link",grid=T)
- #Pretty clearly breaks assumptions, right?
+#Pretty clearly breaks assumptions, right?
 
 #poisglm2 <- glm(cve_bin_tool ~ log(size) +domain+compiler+static, family=quasipoisson,data=data)
-#AIC: NA, still has some issues it seems like
+#AIC: NA, has some issues it seems like
 
-m2 <- zeroinfl(cve_bin_tool ~ log(size) +domain+compiler+static|log(size) +domain+compiler+static,data=data, dist="poisson")
-AIC(poisglm2, m2)
+m2all <- zeroinfl(cve_bin_tool ~ log(size) +domain+compiler+static|log(size) +domain+compiler+static,data=data, dist="poisson")
+AIC(poisglm2, m2all)
 
 
 #Removing lowest coef of non-significant variables, re-running each time until all sig. variables
 m2 <- zeroinfl(cve_bin_tool ~ domain+compiler+static|log(size)+domain,data=data, dist="poisson")
 summary(m2)
+AIC(m2all,m2)
+
+m2nbm <- zeroinfl(cve_bin_tool~log(size) +domain+compiler+static|log(size) +domain,data=data, dist="negbin")
+summary(m2nbm)
+AIC(m2,m2nbm)
 #Seems to be the best
 
-res <- residuals(m2)
-plot(log(predict(m2)), res)
-abline(h=0, lty=2)
-#Better at least?
-
-
+logModel <- glm(cbtFindings ~ log(size) +domain,data=data,family="binomial")
+summary(logModel)
+plot(logModel)
 
 ##Yara Rules Model Fitting###############################################
 poisglm3 <- glm(yara_rules ~ log(size) +domain+compiler+static, family=poisson(link="log"),data=data)
-
 summary(poisglm3)
 plot(allEffects(poisglm3,resid=T),type="link",grid=T)
 par(mfrow=c(2,2))
 plot(poisglm3)
 
-m3all <- zeroinfl(yara_rules ~ log(size) +domain+compiler+static|log(size) +domain+compiler+static, dist="poisson",data=data)
-AIC(poisglm3, m3all)#m3all better
+dispersiontest(poisglm3)
+
+#Check if offset model is better
+poisglm3offset <- glm(yara_rules ~ offset(log(size)) +domain+compiler+static, family=poisson(link="log"),data=data)
+AIC(poisglm3,poisglm3offset) #Offset not better
+
+#Fit NBM, check if better
+nbm1 <- glm.nb(yara_rules ~ log(size) +domain+compiler+static, data=data)
+summary(nbm1)
+AIC(poisglm3,nbm1) #Performs better than Poisson
+
+
+
+zeroInflatedPois <- zeroinfl(yara_rules ~ log(size) +domain+compiler+static|log(size) +domain+compiler+static, dist="poisson",data=data)
+AIC(poisglm3, zeroInflatedPois,nbm1)#zeroinf better. but only barely
+
+m3nbm <- zeroinfl(yara_rules ~ log(size) +domain+compiler+static|log(size) +domain+compiler+static, dist="negbin",data=data)
+AIC(m3nbm, m3all)#m3all better, stick with poisson
+
 
 #Removing lowest coef of non-significant variables, re-running each time until all sig. variables
-m3 <- zeroinfl(yara_rules ~ log(size) +domain+compiler|log(size)+static, dist="poisson",data=data)
-AIC(m3,m3all) #m3 better
+m3 <- glm.nb(yara_rules ~ log(size) +domain+static, data=data)
 summary(m3)
 
-res <- residuals(m3)
-plot(log(predict(m3)), res)
+res <- residuals(m3nbm)
+plot(log(predict(m3nbm)), res)
 abline(h=0, lty=2)
-#Better? Unsure.
+#A little bit better
 
 #Interpretation####################################
 ## Exponentiated coefficients, cve-bin-tool
@@ -108,7 +128,4 @@ expCoefm2
 
  ## Exponentiated coefficients, yara-rules
 expCoefm3 <- exp(coef((m3)))
-expCoefm3 <- matrix(expCoefm3,ncol=2)
-rownames(expCoefm3) <- names(coef(m1))
-colnames(expCoefm3) <- c("Count_model","Zero_inflation_model")
 expCoefm3
